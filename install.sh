f9ba7c68621ce8e0e65dec64d84ad46dfca27165
vreguraman <96850237+vreguraman@users.noreply.github.com>
2025-03-17 16:32:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt install zip
+apt install zip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
@@ -8,3 +8,11 @@ sudo ./aws/install
 wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
+
+
+###valut
+
+sudo apt update && sudo apt install gpg
+
+
+vault server -dev -dev-listen-address="0.0.0.0:8200"
