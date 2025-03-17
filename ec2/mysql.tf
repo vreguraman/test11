3cb8378a434db73272cea77fd51c4ee208042377
vreguraman <96850237+vreguraman@users.noreply.github.com>
2025-03-17 12:48:08 +0000

    Update mysql.tf

--- ec2/mysql.tf
+++ ec2/mysql.tf
@@ -1,1 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_db_instance" "example1" {
+  identifier           = "example1-db"
+  engine               = "mysql"
+  engine_version       = "8.4.3"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 10  # Required field
+  username             = "admin"
+  password             = "supersecretpassword"
+  db_subnet_group_name = "db"
+  skip_final_snapshot  = true  # Skip final snapshot when destroying the instance
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.example1.endpoint
+}
